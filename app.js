185d55fad1ceca6910b9d6cd6a0e4ed1ab514702
LoreMolinari <lore.molinari.00@gmail.com>
2023-03-18 16:01:54 +0100

    Diff-Algo
    First implementation of diff algo

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+require('colors');
+const Diff = require('diff');
+
 const fs = require("fs");
 require("dotenv").config();
 
@@ -19,10 +22,35 @@ app.get("/firepad", (req, res) => {
   res.render("firepad");
 });
 
+app.get("/diff", (req, res) => {
+  res.render("diff");
+});
+
 app.get("/load", (req, res) => {
   let annotations = fs.readFileSync("./example.json", "utf-8");
 
   res.json(JSON.parse(annotations));
 });
+
+
+/*app.post('/diff', function(request, response){
+  var recogito = request.body.documents.documentRecogito;
+  var firepad = request.body.documents.documentFirepad;
+
+  //console.log(recogito);
+  //console.log(firepad);
+
+  const diff = Diff.diffChars(recogito, firepad);
+
+  diff.forEach((part) => {
+    // green for additions, red for deletions
+    // grey for common parts
+    const color = part.added ? 'green' :
+      part.removed ? 'red' : 'grey';
+      //process.stderr.write(part.value[color]);
+  });
+
+  response.json(diff);
+});*/
 
 app.listen(process.env.PORT);
