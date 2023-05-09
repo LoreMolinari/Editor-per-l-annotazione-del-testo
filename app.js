e3ed40adeb94a39f5f98ab1253f3397a8232669a
LoreMolinari <lore.molinari.00@gmail.com>
2023-05-09 10:25:53 +0200

    Bugfixes
    Bugfixes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 
 require("colors");
 const moment = require("moment");
+var createError = require('http-errors');
 
 const fs = require("fs");
 const path = require("path");
@@ -132,4 +133,21 @@ const saveChanges = (nameFile = fileName) => {
   );
 };
 
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
 app.listen(process.env.PORT);
+console.log("Server in ascolto sulla porta: " + process.env.PORT);
